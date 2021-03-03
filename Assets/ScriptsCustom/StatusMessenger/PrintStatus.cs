65e91db75348d86a69135f00deee1f3925eeb0c3
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-03 21:20:10 +0100

    adds printStatus

---
+++ Assets/ScriptsCustom/StatusMessenger/PrintStatus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// print the status that was sent by the server 
+// 
+public class PrintStatus : MonoBehaviour
+{
+    public string statusEventName;
+    private TextMeshPro statusTextManager;
+    void Awake()
+    {
+        statusTextManager = this.gameObject.GetComponent<TextMeshPro>();
+        
+    }
+
+    // Update is called once per frame
+    void OnEnable()
+    {
+
+        EventManager.StartListening(statusEventName, setNewStatus);
+
+    }
+    void OnDisable()
+    {
+
+        EventManager.StartListening(statusEventName, setNewStatus);
+    }
+    
+    void setNewStatus(EventParam newStatus)
+    {
+        statusTextManager.text = newStatus.status;
+    }
+}
