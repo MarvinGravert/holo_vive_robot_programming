141b1ec2d9ba9db0333d6014f969bee1dfb6b4e6
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-04 03:11:48 +0100

    adds moving script

---
+++ Assets/ScriptsCustom/StatusMessenger/NotifyPoseEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NotifyPoseEvent : MonoBehaviour
+{
+    public string objectPoseInformationEventName;
+    private EventParam pose;
+    public void notifyPose()
+    {
+        Vector3 position=this.gameObject.transform.position;
+        Quaternion rotation = this.gameObject.transform.rotation;
+
+        pose = new EventParam();
+        
+    }
+}
