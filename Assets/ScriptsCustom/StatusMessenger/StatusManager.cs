e0d8609dc78632978de8d39462312afeeef9c6d8
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-10 14:23:01 +0100

    working status manager

---
+++ Assets/ScriptsCustom/StatusMessenger/StatusManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class StatusManager : MonoBehaviour
+{
+    public string statusEventName;
+    public GameObject DebugLogger;
+   
+
+    void OnEnable()
+    {
+
+        EventManager.StartListening(statusEventName, ManageStatus);
+
+    }
+    //private void Update()
+    //{
+    //    DebugLogger.SetActive(false);
+    //}
+    void OnDisable()
+    {
+
+        EventManager.StartListening(statusEventName, ManageStatus);
+    }
+
+    void ManageStatus(EventParam newStatus)
+    {
+        Debug.Log("so its go wit");
+        string status = newStatus.status;
+        if (status.Contains("cmd")){//cmd_order_setting
+            var parts=status.Split('_');
+            string order = parts[1];
+            var setting = parts[2];
+            Debug.Log(setting);
+            switch (order)
+            {
+                case "debug":
+                    if (setting.Contains("on"))
+                    {
+                        Debug.Log("turning on");
+                        DebugLogger.SetActive(true);
+                    }
+                    if (setting.Contains("off"))
+                    {
+                        Debug.Log("turning off");
+                        DebugLogger.SetActive(false);
+                    }
+                    break;
+            }
+
+        }
+            
+    }
+}
