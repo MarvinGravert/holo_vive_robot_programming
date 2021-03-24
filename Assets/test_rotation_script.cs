d4574b790928a570dd71d033f7c2d1818ceffac9
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-24 18:05:42 +0100

    added debugging rotation script

---
+++ Assets/test_rotation_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_rotation_script : MonoBehaviour
+{
+    public GameObject testpoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.transform.position.ToString("F4"));
+        Debug.Log(this.transform.rotation.ToString("F6"));
+        Debug.Log($"Point: {testpoint.transform.position.ToString("F4")}");
+    }
+}
